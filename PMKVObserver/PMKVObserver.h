5417a50645102806c391c6a6e714811ba90de1d2
Kevin Ballard <kevin@sb.org>
2015-11-18 18:06:37 -0800

    Initial commit

---
+++ PMKVObserver/PMKVObserver.h
@@ -0,0 +1,19 @@
+//
+//  PMKVObserver.h
+//  PMKVObserver
+//
+//  Created by Kevin Ballard on 11/18/15.
+//  Copyright © 2015 Kevin Ballard. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for PMKVObserver.
+FOUNDATION_EXPORT double PMKVObserverVersionNumber;
+
+//! Project version string for PMKVObserver.
+FOUNDATION_EXPORT const unsigned char PMKVObserverVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PMKVObserver/PublicHeader.h>
+
+
